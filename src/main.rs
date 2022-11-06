fddeb9f9f88bafd44e37c332add2b1ca1cb8e7de
adamcole123 <adamcole123@hotmail.co.uk>
2022-11-06 18:32:22 +0000

    Added new test

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@ fn read_file(p: &str) -> TResult<String> {
     read_to_string(p).map_err(|e| e.into())
 }
 
+fn split_numbers(s: &str) -> TResult<Vec<usize>> {
+    s.split_whitespace()
+        .map(|x| x.parse::<usize>().map_err(|e| e.into()))
+        .collect()
+}
 
 fn main() {
     println!("This is a test!");
@@ -22,12 +27,23 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_read_file(){
+    fn test_read_file() {
         let res = read_file("test_data/test_one.txt");
         assert!(res.is_ok());
 
         if let Ok(s) = res {
-            assert_eq!(s, "this is a test file yo\n");
+            assert_eq!(s, "3\n5\n");
+        }
+    }
+
+    #[test]
+    fn test_split_numbers() {
+        let res = split_numbers(&String::from("5\n8"));
+
+        assert!(res.is_ok());
+
+        if let Ok(v) = res {
+            assert_eq!(v, vec![5, 8]);
         }
     }
 }
